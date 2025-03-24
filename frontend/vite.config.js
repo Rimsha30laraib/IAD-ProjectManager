435cf70a3a3b6bc91406ae0edc86e2609c18e960
Rimsha30laraib <rimshalarib33@gmail.com>
2025-03-24 21:39:14 +0500

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env.API_URL': JSON.stringify('https://iad-project-manager-fde9.vercel.app/'),
+  }
 })
+
