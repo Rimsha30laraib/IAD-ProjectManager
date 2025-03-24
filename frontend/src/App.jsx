31cb8f700725639fe3150b36c9db8a7ea98c49c2
Rimsha30laraib <rimshalarib33@gmail.com>
2025-03-24 21:51:23 +0500

    done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
 <h1 style={{ textAlign: "center", margin: "20px auto" }}>Project Management</h1>
-<ProjectManager apiUrl="https://vercel.com/rimshas-projects-9ef85771/iad-project-manager-f9jg/api" />
+<ProjectManager apiUrl="https://iad-project-manager-f9jg.vercel.app/api" />
 {/* <ProjectManager apiUrl="http://localhost:5000/api" /> */}
 
     </div>
