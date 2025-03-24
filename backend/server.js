bc74dbc890d167c38f521193467b0938ffe26749
Rimsha30laraib <rimshalarib33@gmail.com>
2025-03-24 20:33:21 +0500

    vercel

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,13 @@ const cors = require("cors");
 const { v4: uuidv4 } = require("uuid");
 
 const app = express();
-
+app.use(cors(
+  {
+    origin:["https://iad-project-manager-fumy.vercel.app/"],
+    methods:["POST","GET","PUT","DELETE"],
+    credentials:true
+  }
+));
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
